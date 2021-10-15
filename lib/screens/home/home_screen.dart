b5592e1573c18d5a420d2b7c783e94560aab1ff6
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-10-15 12:47:45 +0300

    Restructure the home screen..

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:demi/models/category_model.dart';
 import 'package:demi/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:demi/widgets/widgets.dart';
@@ -35,60 +34,7 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           SectionTitle(title: "RECOMMENDED"),
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width / 2.5,
-                height: 150,
-                child: Image.network(
-                  Product.products[0].imageUrl,
-                  fit: BoxFit.cover,
-                  ),
-              ),
-              Positioned(
-                top: 60,
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 2.5 - 10,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.black.withAlpha(50),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 65,
-                left: 5,
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 2.5 - 10,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              Product.products[0].name,
-                              style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),
-                            ),
-                            Text(
-                              '\$${Product.products[0].price}',
-                              style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),
-                            ),
-                          ],
-                        )
-                      ],
-                      ),
-                  )
-                ),
-              )
-            ],
-          ),
+          ProductCard(product: Product.products[0]),
         ],
       ),
     );
